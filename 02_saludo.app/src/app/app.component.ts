a1a2b5a6880670fd16e154deb47c9ec986dbae11
Enrique Revelo <mreveloescobar@gmail.com>
2025-04-30 09:51:33 +0200

    origin

---
+++ 02_saludo.app/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  data:any;
+mensaje():void{
+// const descuento:number=10;
+// a veces no recoge el tipo
+ let caja:HTMLInputElement=document.getElementById("nombre");
+}
+}
