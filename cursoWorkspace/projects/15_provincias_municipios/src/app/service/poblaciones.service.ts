64d16c04e0429813c19466eef901b3857d3f3f80
Enrique Revelo <mreveloescobar@gmail.com>
2025-05-14 12:40:51 +0200

    mañana 14 mayo

--- cursoWorkspace/projects/15_provincias_municipios/src/app/service/poblaciones.service.ts
+++ cursoWorkspace/projects/15_provincias_municipios/src/app/service/poblaciones.service.ts
@@ -10,14 +10,37 @@ export class PoblacionesService {
 urlProvincias= "https://www.el-tiempo.net/api/json/v2/provincias/01/municipios"
   constructor(private http: HttpClient) { }
 }
-provincias():Observable<Provincia[]>{
-return this.http.get<any>(this.urlProvincias)
-.pipe(map(obj=>obj.provincias))
-}
 
-municipiosProvincia(cod:number):Observable<Municipio[]>{
-  
-}
+ constructor(private http:HttpClient) {
+   }
+  provincias()Observable<Provincia[]>{
+    return this.http.get<any>(this.urlProvincias)
+    .pipe(map(obj=>obj.provincias));
+
+  }
+  municipiosProvincia(cod:number)😮bservable<Municipio[]>{
+    const url string=`${this.urlProvincias}/${cod}/municipios`;
+    return this.http.get<any>(url)
+    .pipe(map(datos=>datos.municipios));
+  }
+
+
+
+
+// provincias():Observable<Provincia[]>{
+// return this.http.get<any>(this.urlProvincias)
+// .pipe(map(obj=>obj.provincias))
+// }
+
+// municipiosProvincia(cod:number):Observable<Municipio[]>{
+
+// }
+
+
+
+
+
+
 
 //export class PaisesService {
   // url:string= "https://restcountries.com/v2/all";
